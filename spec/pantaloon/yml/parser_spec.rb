8fe9f1a12e08aa3620299a297ed66c5addebb683
Arvind Kunday <arvind.kunday@rea-group.com>
2014-12-20 22:43:25 +1100

    Cleanup project structure.

--- spec/pantaloon/yml/parser_spec.rb
+++ spec/pantaloon/yml/parser_spec.rb
@@ -1,3 +1,10 @@
-describe Pantaloon::Yml::Parser do
-  let :config { File.read("spec/fixtures/pantaloon.yml") }
+require File.expand_path('../../../../lib/pantaloon/yml/configuration', __FILE__)
+
+describe Pantaloon::Yml::Configuration do
+  let(:config) { File.read("spec/fixtures/pantaloon.yml") }
+  let(:subject) { Pantaloon::Configuration.new(config) }
+
+  it "should get the right git url" do
+    expect(subject.url).to eq("https://git.realestate.com.au/akunday/cp-domain/")
+  end
 end
