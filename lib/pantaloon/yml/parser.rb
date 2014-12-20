496e1dafd891fb098c782e9b9cb13e8053265c05
Arvind Kunday <arvind.kunday@rea-group.com>
2014-12-20 15:54:38 +1100

    Initial Commit.

---
+++ lib/pantaloon/yml/parser.rb
@@ -0,0 +1,9 @@
+require "pantaloon/yml/parser/version"
+
+module Pantaloon
+  module Yml
+    class Parser
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/pantaloon/yml/parser/version.rb
@@ -0,0 +1,7 @@
+module Pantaloon
+  module Yml
+    module Parser
+      VERSION = "0.0.1"
+    end
+  end
+end
