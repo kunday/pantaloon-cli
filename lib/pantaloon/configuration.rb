6e9aa17df3ffe846ac065dd3ee973907432f0fb8
Arvind Kunday <kunday@gmail.com>
2014-12-21 10:17:32 +1100

    Dry run config.

--- lib/pantaloon/configuration.rb
+++ lib/pantaloon/configuration.rb
@@ -1,18 +1,17 @@
+require 'dish/ext'
 require 'yaml'
+require 'erb'
 
 module Pantaloon
   class Configuration
     attr_accessor :config
     def initialize(config)
-      @config = YAML.load(config)
+      @config = Dish(YAML.load(config))
     end
 
-    def url
-      config["url"]
-    end
-
-    def repo
-      config["repo"]
+    def build_step
+      build_template = File.read(File.expand_path("../../../lib/pantaloon/templates/build-step.erb", __FILE__))
+      ERB.new(build_template).result(build_template.send(:binding))
     end
   end
 end
