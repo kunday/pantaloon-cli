75d1cf24639a4f2ececa952ddf5cdaf3a0d59c25
Arvind Kunday <kunday@gmail.com>
2014-12-21 11:01:57 +1100

    project-view template is now complete.

--- lib/pantaloon/configuration.rb
+++ lib/pantaloon/configuration.rb
@@ -6,11 +6,16 @@ module Pantaloon
   class Configuration
     attr_accessor :config
     def initialize(config)
-      @config = Dish(YAML.load(config))
+      @config = YAML.load(config)
     end
 
     def build_step
       build_template = File.read(File.expand_path("../../../lib/pantaloon/templates/build-step.erb", __FILE__))
+      ERB.new(build_template).result(build_template.send(:binding))
+    end
+
+    def project_view
+      build_template = File.read(File.expand_path("../../../lib/pantaloon/templates/project-view.erb", __FILE__))
       ERB.new(build_template).result(build_template.send(:binding))
     end
   end
